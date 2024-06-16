e599f4ca761f1b1c7ac1cbc7f3a8538310d02396
Sripriya Ragunath <sripriyar@infinite.com>
2024-06-16 22:43:28 +0530

    assignments

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter the first integer : ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(" enter the second integer : ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 == num2 )
+        {
+            Console.WriteLine(" the two integers are equal ");
+        }
+
+        else
+        {
+            Console.WriteLine(" the two integers are not equal ");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ positive negative.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class posneg
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter the number ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine(" the number is positive ");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine(" the number is negative ");
+        }
+        else
+        {
+            Console.WriteLine(" the number is zero ");
+        }
+        Console.ReadLine();
+    }
+}
