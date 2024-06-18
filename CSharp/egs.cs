a6d98c4834aff0c016ab3ca20e55d8a0d3d0a4fd
Sripriya Ragunath <sripriyar@infinite.com>
2024-06-18 20:59:19 +0530

    exercise

---
+++ CSharp/egs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Day4_Dotnet
+{
+    class shape
+    {
+        protected float R, L, B;
+
+        //function to calculate area of shape
+
+        public virtual float Area() 
+        public float Area()
+        {
+            return 3.14f * R * R;
+        }
+    }
+    class Rectabgle : shape
+    {
+        public void GetLB()
+        {
+            Console.Write("Enter Length :");
+            L = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter Breadth : ");
+            B = float.Parse(Console.ReadLine());
+
+        }
+
+        public override float Area()
+        {
+            return L * B ();
+        }
+    }
+    
+
+}
