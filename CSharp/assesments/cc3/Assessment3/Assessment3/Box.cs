93ab06c1a758a39dc6cf510c170a3b1d47acb170
Sripriya Ragunath <sripriyar@infinite.com>
2024-07-03 13:09:26 +0530

    ass3

---
+++ CSharp/assesments/cc3/Assessment3/Assessment3/Box.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Box
+{
+    public int Length
+    { 
+        get;
+        set; 
+    }
+    public int Breadth
+    {
+        get;
+        set;
+    }
+
+    public Box(int length, int breadth)
+    {
+        Length = length;
+        Breadth = breadth;
+    }
+
+    public static Box AddBox(Box box1, Box box2)
+    {
+        Box box3 = new Box(box1.Length + box2.Length, box1.Breadth + box2.Breadth);
+        return box3;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Length: " + Length);
+        Console.WriteLine("Breadth: " + Breadth);
+    }
+}
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter length of Box 1: ");
+        int length1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter breadth of Box 1: ");
+        int breadth1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter length of Box 2: ");
+        int length2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter breadth of Box 2: ");
+        int breadth2 = Convert.ToInt32(Console.ReadLine());
+
+        Box box1 = new Box(length1, breadth1);
+        Box box2 = new Box(length2, breadth2);
+
+        Box box3 = Box.AddBox(box1, box2);
+
+        Console.WriteLine("Box 1 details:");
+        box1.Display();
+
+        Console.WriteLine("Box 2 details:");
+        box2.Display();
+
+        Console.WriteLine("Box 3 details:");
+        box3.Display();
+    }
+}
---
+++ CSharp/assesments/cc3/Assessment3/Assessment3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Cricket
+{
+    public void Pointscalculation(int no_of_matches)
+    {
+        int sum = 0;
+        for (int i = 0; i < no_of_matches; i++)
+        {
+            Console.Write("Enter the scores {0}: ", i + 1);
+            int score = Convert.ToInt32(Console.ReadLine());
+            sum += score;
+        }
+
+        double average = (double)sum / no_of_matches;
+
+        Console.WriteLine("Sum of scores: " + sum);
+        Console.WriteLine("Average score: " + average);
+    }
+}
+
+class Match
+{
+    static void Main(string[] args)
+    {
+        Cricket cricket = new Cricket();
+        Console.WriteLine(" Welcome to cricket world !!! ");
+        Console.Write("Enter the number of matches: ");
+        int no_of_matches = Convert.ToInt32(Console.ReadLine());
+        cricket.Pointscalculation(no_of_matches);
+    }
+}
