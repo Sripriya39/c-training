5f031ee2e6e87f62c488b5cedbc930c8a0695884
Sripriya Ragunath <sripriyar@infinite.com>
2024-07-10 11:18:21 +0530

    Assessment4

---
+++ CSharp/assesments/cc4/assesment4/assesment4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string directoryPath = @" C:\ass4text";
+        string fileName = "ass4txt.txt";
+        string filePath = Path.Combine(directoryPath, fileName);
+        string textToAppend = "hello, Welcome to C sharp Training in Infinite ";
+
+        try
+        {
+            
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine(textToAppend);
+            }
+
+            Console.WriteLine("yeah..!! your text appended successfully on your asstxt4 file.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
---
+++ CSharp/assesments/cc4/assesment4/assesment4/calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Calculator
+{
+    delegate int CalculatorDelegate(int num1, int num2);
+
+    static void Main()
+    {
+        Console.WriteLine("Calculator Functionalities");
+        Console.WriteLine("*******************");
+
+        Console.Write("Enter the first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        CalculatorDelegate addDelegate = Add;
+        CalculatorDelegate subtractDelegate = Subtract;
+        CalculatorDelegate multiplyDelegate = Multiply;
+
+        int resultAdd = addDelegate(num1, num2);
+        int resultSubtract = subtractDelegate(num1, num2);
+        int resultMultiply = multiplyDelegate(num1, num2);
+
+        
+        Console.WriteLine($"Addition: {num1} + {num2} = {resultAdd}");
+        Console.WriteLine($"Subtraction: {num1} - {num2} = {resultSubtract}");
+        Console.WriteLine($"Multiplication: {num1} * {num2} = {resultMultiply}");
+    }
+
+    static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    static int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    static int Multiply(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+}
